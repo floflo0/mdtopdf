a49bd23bc0172e0006361f825a72857acb590331
Floris Bartra <floris.bartra@gmail.com>
2022-10-29 13:54:36 +0200

    Deb package

--- package-deb.sh
+++ package-deb.sh
@@ -3,32 +3,31 @@
 set -e
 
 PKGNAME="mdtopdf"
-PKGDESC="Convert markdown file to pdf file using chromium."
 PKGVER=$(python -c "print(__import__('${PKGNAME}').__version__)")
-
+PKGREL="1"
 PKGDIR="pkg"
+ARCH="amd64"
 
-mkdir --verbose "${PKGDIR}"
-if [ -d "${PKGDIR}" ]; then
+if [ -e "${PKGDIR}" ]; then
     rm --verbose --recursive "${PKGDIR}"
 fi
 mkdir --verbose "${PKGDIR}"
 
-mkdir --parents --verbose "${PKGDIR}/usr/bin"
-cp --verbose mdtopdf.py "${PKGDIR}/usr/bin/${PKGNAME}"
-chmod --verbose +x "${PKGDIR}/usr/bin/${PKGNAME}"
+install --verbose -Dm 755 "${PKGNAME}.py" "${PKGDIR}/usr/bin/${PKGNAME}"
 
 mkdir --parents --verbose "${PKGDIR}/DEBIAN"
 cat << EOF > "${PKGDIR}/DEBIAN/control"
 Package: ${PKGNAME}
 Name: MdToPdf
-Description: ${PKGDESC}
+Description: Convert markdown file to pdf file using chromium.
 Maintainer: Floris Bartra <floris.bartra@gmail.com>
 Version: ${PKGVER}
-Architecture: all
+Architecture: ${ARCH}
 Depends: python3, python3-markdown, python3-pygments, chromium-browser
+Homepage: https://github.com/floflo0/mdtopdf
+Provides: ${PKGNAME}
 EOF
 
-dpkg-deb --verbose --build "${PKGDIR}" "${PKGNAME}-${PKGVER}.deb"
+dpkg-deb --verbose --build "${PKGDIR}" "${PKGNAME}_${PKGVER}-${PKGREL}_${ARCH}.deb"
 
 rm --verbose --recursive "${PKGDIR}"
